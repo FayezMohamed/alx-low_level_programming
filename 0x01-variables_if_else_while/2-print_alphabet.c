65ff9b53e9a37e9feda3145938f2fa182f62ff4d
FayezMohammed <fayezmfayez@gmail.com>
2023-08-14 16:05:01 -0700

    2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alphabet[] = "abcdefghijklmnpqrstuvwxyz";
+	int i;
+	for(i = 0; i<26; i++)
+	{
+	putchar(alphabet[i]);
+	}
+
+	putchar("\n")
+	return (0);
+}
+
