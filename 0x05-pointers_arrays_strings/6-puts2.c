3881d404cb85dfe17f59be5debf8d8baf9eea969
FayezMohammed <fayezmfayez@gmail.com>
2023-08-21 18:21:55 -0700

    6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -3,24 +3,26 @@
  * puts2 - prints every charater in a string
  * @str: string
  */
-void puts2(char *str)
+void puts_half(char *str)
 {
 	int i;
-    	int length = 0;
+	int j;
+	int length = 0;
 
-    	while (*str != '\0')
-    	{
-        	length++;
-        	str++;
-    	}
+	while (*str != '\0')
+	{
+		length++;
+		str++;
+	}
+		j = length -1;
 
-    	for (i = 0; i < length; i++)
-    	{
+	for (i = 0; i <= j; i++)
+	{
 		if (i % 2 == 0)
 		{
-        	_putchar(str[i]);
+			_putchar(str[i]);
 		}
-    	}
+	}
 	_putchar('\n');
 }
 
