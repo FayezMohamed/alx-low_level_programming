c11a7472ddf3f0fc8b09afcfa42d16a4fdb48ee6
FayezMohammed <fayezmfayez@gmail.com>
2023-08-21 18:46:29 -0700

    7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * puts_half - prints half a string
+ * @str: string
+ */
+void puts_half(char *str)
+{
+	char *y = str;
+	int length = 0;
+	int half;
+	int i;
+	int n;
+
+	while (*y != '\0')
+	{
+		length++;
+		y++;
+	}
+	half = length / 2;
+	n = (length - 1) / 2;
+
+	if (half % 2 == 0)
+	{
+		for (i = half; i < length; i++)
+		{
+			_putchar(str[i]);
+		}
+	else
+	{
+		for (i = n; i < length; i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+}
