65d3b33ef4cce44df1b9b5fba80bbc53626fb143
FayezMohammed <fayezmfayez@gmail.com>
2023-08-24 17:44:58 -0700

    1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * *_strncat - appends a string to another using n bytes from src
+ * @src: string
+ * @dest: string
+ * Return: updated dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	int j = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	while (src[j] != '\0' && j < n)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
