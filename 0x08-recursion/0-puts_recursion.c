cc836dbf5a83a460978141e3b488b278c54b10d1
root <fayezmfayez@gmaul.com>
2023-08-31 19:50:36 -0700

    0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _puts_recursion - prints a string and new line
+ * _puts_recursion - prints a string and new line (puts)
  * @s: string
  */
 void _puts_recursion(char *s)
