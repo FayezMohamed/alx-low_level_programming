6c992d36f446abe5553959d5cb2e0a737dfa977f
FayezMohamed <fayezmfayez@gmail.com>
2023-09-26 19:29:36 -0700

    4

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,12 +1,11 @@
 #include "lists.h"
-
 /**
  * free_listint - frees a linked list
  * @head: linked list
  */
 void free_listint(listint_t *head)
 {
-	listint_t *current'
+	listint_t *current;
 	while (head)
 	{
 		current = head->next;
