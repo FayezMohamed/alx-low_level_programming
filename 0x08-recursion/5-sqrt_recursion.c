176ea4e4fffbde0c402a588de58de87d22d846e1
FayezMohamed <fayezmfayez@gmail.com>
2023-10-25 15:30:18 -0700

    6

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _sqrt_recursion - calc square root of a number
+ * @n: integer
+ *
+ * Return: result
+ */
+int _sqrt_recursion(int n)
+{
+
+}
---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * is_prime_number - checks whether number is prime
+ * @n: integer
+ *
+ * Return: 1 if prime 0 if not
+ */
+
+int sumDigits(int n)
+{
+	if (n == 0)
+	{
+		return 0;
+	}
+	return (n % 10) + sumDigits(n / 10);
+}
+int is_prime_number(int n)
+{
+	if (n == 2 || n == 3)
+	{
+		return 1;
+	}
+
+	if (sumDigits(n) % 3 == 0)
+	{
+		return 1;
+	}
+	
+	else (return 0);
+
+}
