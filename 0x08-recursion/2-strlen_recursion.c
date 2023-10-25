433be1e6f43bf3a9ae06837e0e4599fac26dca8d
FayezMohamed <fayezmfayez@gmail.com>
2023-10-25 14:08:33 -0700

    2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * _strlen_recursion - returns the length of a string
- * @s: string
+ *_strlen_recursion - returns the length of a string
+ *@s: string
  *
- * Return: integer
+ *Return: integer
  */
 
 int _strlen_recursion(char *s)
