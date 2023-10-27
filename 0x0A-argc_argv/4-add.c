911c6fab16c9fef5617c33bd90642cbe3c7fe0cc
FayezMohamed <fayezmfayez@gmail.com>
2023-10-27 16:10:08 -0700

    4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -13,18 +13,37 @@ int main(int argc, char *argv[])
 
 	if (argc < 1)
 	{
-		return (0);
+		printf("%d\n", 0);
 	}
+	
 
-	for (i = 1; i < argc; i++)
-	{
-		sum += atoi(argv[i]);
-                if (!atoi(argv[i]))
-                {
-                        printf("%s\n", "Error");
-                        return (1);
-                }	
-	}
-	printf("%d\n", sum);
-	return (0);
+    	sum = 0;
+
+    	for (i = 1; i < argc; i++)
+    	{
+        	char *arg = argv[i];
+        	int j = 0;
+
+        	while (arg[j] != '\0')
+        	{
+            		if (arg[j] < '0' || arg[j] > '9')
+            		{
+                		printf("Error\n");
+                		return 1;
+            		}
+            		j++;
+        	}
+
+        	int num = atoi(arg);
+        	if (num < 0)
+        		{
+            			printf("Error\n");
+            			return 1;
+        		}
+
+        	sum += num;
+    	}
+
+    	printf("%d\n", sum);
+    	return 0;
 }
