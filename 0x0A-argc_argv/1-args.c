f007f9ca50ad392885523817264870d3335c6ff7
FayezMohamed <fayezmfayez@gmail.com>
2023-10-27 15:05:30 -0700

    1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - prints the number of arguments passed into it followed by a new line
+ * @argc: number of arguments typed
+ * @argv: array pointing to arguements
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d/n", argc);
+	return (0);
+}
