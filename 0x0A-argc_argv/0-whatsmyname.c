0197583d450924b3165eda1578f3f770a349961c
FayezMohamed <fayezmfayez@gmail.com>
2023-10-27 14:49:52 -0700

    0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * mainh - prints its name and a new line
+ * @argc: number of arguments
+ * @argv: array for the arguments
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s/n", argv[0]);
+}
