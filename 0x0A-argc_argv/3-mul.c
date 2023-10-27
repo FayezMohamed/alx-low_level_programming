79d4da89b1bf77b315208b3133473118f335b777
FayezMohamed <fayezmfayez@gmail.com>
2023-10-27 15:35:30 -0700

    3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -14,12 +14,12 @@ int main(int argc, char *argv[])
 
 	if (argc == 3)
 	{
-        int num1 = atoi(argv[1]);
-        int num2 = atoi(argv[2]);
+        	int num1 = atoi(argv[1]);
+        	int num2 = atoi(argv[2]);
 
-        product = num1 * num2;
-        printf("%d\n", product);
-        return 0;
+        	product = num1 * num2;
+        	printf("%d\n", product);
+        	return 0;
 	}
 	else
 	{
