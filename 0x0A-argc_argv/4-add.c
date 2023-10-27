d1d9a4bd7d89201cedebfc9968ce49df40d4bda0
FayezMohamed <fayezmfayez@gmail.com>
2023-10-27 15:49:05 -0700

    4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+/**
+ * main - prints sum of positive numbers
+ * @argc: number of arguments
+ * @argv: array pointing to arguments
+ * Return: 0 if numbers are passed, 1 if any argument has symbols
+ */
+int main(int argc, char *argv[])
+{
+	int i, sum;
+	if (argc < 1)
+	{
+		return (0);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		sum += atoi(argv[i]);
+		if (!atoi(argv[i]))
+		{
+			printf("%s\n", "Error");
+			return (1);
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
