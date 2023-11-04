716de17d85f30e51ca2fc03c18fe14db5a0f89f6
FayezMohamed <fayezmfayez@gmail.com>
2023-11-03 19:03:42 -0700

    1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -36,13 +36,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		n = n2;
 	}
-
 	s = malloc((n1 + n + 1) * sizeof(char));
 	if (s == NULL)
 	{
 		return (NULL);
 	}
-
 	for (i = 0; i < n1; i++)
 	{
 		s[i] = s1[i];
