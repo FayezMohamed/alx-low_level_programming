cdd1dc6c51fbf1faaccf672ef5f1d9a2ca56c802
FayezMohamed <fayezmfayez@gmail.com>
2023-11-03 19:28:20 -0700

    2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,6 +11,8 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *arr;
+	unsigned int total_size, i;
+	char *temp = (char *)arr;
 
 	arr = malloc(nmemb * size);
 	if (nmemb == 0 || size == 0)
@@ -20,6 +22,10 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (arr == NULL)
 	{
 		return (NULL);
+	}
+	for (i = 0; i < total_size; i++)
+	{
+		temp[i] = 0;
 	}
 	return (arr);
 }
