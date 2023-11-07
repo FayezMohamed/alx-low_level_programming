a95427af3858c2e3bf22764d2ce1e9e867618117
FayezMohamed <fayezmfayez@gmail.com>
2023-11-06 18:01:41 -0800

    0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - struct
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: owner's name
+ */
+typedef struct dog
+{
+	char *name;
+	float *age;
+	char *owner;
+}
+
+#endif
