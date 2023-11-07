14c49b4420568623781d156b04705c753a24fe31
FayezMohamed <fayezmfayez@gmail.com>
2023-11-06 17:30:18 -0800

    4

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,11 @@
+#ifndef MACRO_H
+#define MACRO_H
+/**
+ * 4-sum.h - header file
+ * SUM - function-like macro that computes the sum of the numbers x and y
+ * @x: integer
+ * @y: integer
+ */
+#define SUM(x, y) ((x) + (y))
+
+#endif
