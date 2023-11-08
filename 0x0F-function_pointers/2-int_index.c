260ece28686fdad36359dd98736c076d4040a86f
FayezMohamed <fayezmfayez@gmail.com>
2023-11-08 13:35:24 -0800

    2

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,27 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - returns the index of the first element
+ * @array: array
+ * @size: size of the array
+ * @cmp: function pointer that compares values
+ *
+ * Retrn: -1 if no element matches or size <= 0
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+
+	if (array && size && cmp)
+	{
+		while (i < size)
+		{
+			if (cmp(array[i]))
+			{
+				return (i);
+			}
+			i++;
+		}
+	}
+	return (-1);
+}
