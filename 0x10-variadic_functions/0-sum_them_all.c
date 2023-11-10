1e4067921b91bf6dc89ca7281d1ec53242fc8240
FayezMohamed <fayezmfayez@gmail.com>
2023-11-10 10:48:19 -0800

    0

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,13 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - calculate sum of all its parameters
+ * @n: number of arguments
+ *
+ * Return: sum
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+
+}
