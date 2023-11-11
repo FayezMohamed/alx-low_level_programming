8699c6ff1e8be7540c2039521944dd1aab87e984
FayezMohamed <fayezmfayez@gmail.com>
2023-11-10 17:12:01 -0800

    0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -9,5 +9,19 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
+	int sum = 0;
+	int i;
+	va_list ap;
 
+	if (n == NULL)
+	{
+		return (0);
+	}
+	va_start(ap, n);
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(ap, int)
+	}
+	va_end(ap);
+	return (sum);
 }
