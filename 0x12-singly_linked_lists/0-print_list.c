a97bea93ae14b6b7e5f111c5d98b2047df56c558
FayezMohamed <fayezmfayez@gmail.com>
2023-11-16 18:16:08 -0800

    0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,7 +9,6 @@
  * 
  * Return: Number of nodes
  */
-
 size_t print_list(const list_t *h)
 {
 	size_t n = 0;
@@ -22,7 +21,7 @@ size_t print_list(const list_t *h)
 		}
 		else
 		{
-                	printf("[%u] %s\n", h->len, h->str);
+			printf("[%u] %s\n", h->len, h->str);
 		}
 		h = h->next;
 		n++;
