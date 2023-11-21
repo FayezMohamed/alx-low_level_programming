35819e70aa18efbf5d6cfcc7a0022c86638fb9b8
FayezMohamed <fayezmfayez@gmail.com>
2023-11-21 10:42:58 -0800

    1

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * listint_len - returns the number of elements in the list
+ * @h: linked list
+ *
+ * Return: number of elements
+ */
+size_t listint_len(const listint_t *h)
+{
+	int length = 0;
+
+	while (h != NULL)
+	{
+		length++;
+		h = h->next
+	}
+	return (length);
+}
