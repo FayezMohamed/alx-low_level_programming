404fb58c4e37aa0e7b10c2df2e2119bd8e3fb145
FayezMohamed <fayezmfayez@gmail.com>
2023-12-01 18:23:56 -0800

    1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 /**
  * create_file - creates a file
  * @filename: name of the file
@@ -7,14 +8,16 @@
  * Return: 1 on success, -1 on failure
  */
 int create_file(const char *filename, char *text_content) {
+	int file_descriptor;
+	ssize_t bytes_written;
     if (filename == NULL) return -1;
 
-    int file_descriptor = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+    file_descriptor = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
 
     if (file_descriptor == -1) return -1;
 
     if (text_content != NULL) {
-        ssize_t bytes_written = write(file_descriptor, text_content, strlen(text_content));
+        bytes_written = write(file_descriptor, text_content, strlen(text_content));
 
         if (bytes_written == -1) {
             close(file_descriptor);
