8884e4cb4f6730e969cad81795783de44c066b9a
FayezMohamed <fayezmfayez@gmail.com>
2023-12-01 18:03:53 -0800

    0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,27 +1,24 @@
 #include "main.h"
 /**
- * read_textfile - reads a text file and prints to POSIX standard output
- * @filename: text file
- * @letters: number of letters to be printed
- *
- * Return: number of letters or 0 if NULL
+ * read_textfile -  prints text fileto the POSIX standard output
  */
-ssize_t read_textfile(const char *filename, size_t letters)
-{
-	int file_descriptor = open(filename, 0_RDONLY);
-	if (filename == NULL)
-	{
-		return (0);
-	}
+
+ssize_t read_textfile(const char *filename, size_t letters) {
+    if (filename == NULL) {
+        exit(0);
+    }
+
+    int file_descriptor = open(filename, O_RDONLY);
+
     if (file_descriptor == -1) {
-        return 0;
+        exit(0);
     }
 
     char *buffer = malloc(letters);
 
     if (buffer == NULL) {
         close(file_descriptor);
-        return 0;
+        exit(0);
     }
 
     ssize_t bytes_read = read(file_descriptor, buffer, letters);
@@ -29,7 +26,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
     if (bytes_read == -1) {
         free(buffer);
         close(file_descriptor);
-        return 0;
+        exit(0);
     }
 
     ssize_t bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
@@ -38,9 +35,9 @@ ssize_t read_textfile(const char *filename, size_t letters)
     close(file_descriptor);
 
     if (bytes_written == -1 || bytes_written != bytes_read) {
-        return 0;
+        exit(0);
     }
 
     return bytes_written;
 }
-}
+
