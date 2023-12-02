444e0fc0e610f3a401c9f73d30c37318b2e28c66
FayezMohamed <fayezmfayez@gmail.com>
2023-12-01 18:20:22 -0800

    1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * create_file - creates a file
+ * @filename: name of the file
+ * @text_contect: string
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content) {
+    if (filename == NULL) return -1;
+
+    int file_descriptor = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+
+    if (file_descriptor == -1) return -1;
+
+    if (text_content != NULL) {
+        ssize_t bytes_written = write(file_descriptor, text_content, strlen(text_content));
+
+        if (bytes_written == -1) {
+            close(file_descriptor);
+            return -1;
+        }
+    }
+
+    close(file_descriptor);
+    return 1;
+}
+
