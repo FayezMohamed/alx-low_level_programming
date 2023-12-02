093380dd277db629928ed5af0b8850b8ed0b0477
FayezMohamed <fayezmfayez@gmail.com>
2023-12-01 18:17:02 -0800

    0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -8,24 +8,28 @@
  */
 
 ssize_t read_textfile(const char *filename, size_t letters) {
+	int file_descriptor;
+    	char *buffer;
+    	ssize_t bytes_read;
+    	ssize_t bytes_written;
     if (filename == NULL) {
         return 0;
     }
 
-    int file_descriptor = open(filename, O_RDONLY);
+    file_descriptor = open(filename, O_RDONLY);
 
     if (file_descriptor == -1) {
         return 0;
     }
 
-    char *buffer = malloc(letters);
+    buffer = malloc(letters);
 
     if (buffer == NULL) {
         close(file_descriptor);
         return 0;
     }
 
-    ssize_t bytes_read = read(file_descriptor, buffer, letters);
+    bytes_read = read(file_descriptor, buffer, letters);
 
     if (bytes_read == -1) {
         free(buffer);
@@ -33,7 +37,7 @@ ssize_t read_textfile(const char *filename, size_t letters) {
         return 0;
     }
 
-    ssize_t bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+    bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
 
     free(buffer);
     close(file_descriptor);
